2f51da09d4290752802c2bd07ebd53011526c422
AMo <115477511+muamelAmeer@users.noreply.github.com>
2023-06-25 21:28:49 +0300

    Update __init__.py

--- userapilib/__init__.py
+++ userapilib/__init__.py
@@ -3,20 +3,20 @@ __author__ = "Muamel Ameer"
 __copyright__ = "Copyright 2023 Muamel Ameer"
 __version__ = "0.0.1"
 __github__ = "https://github.com/muamelameer/userapi"
-__pypi__ = "https://pypi.python.org/pypi/userapi"
+# __pypi__ = "https://pypi.python.org/pypi/userapi"
 from .userapi import *
 from requests import get
-__newest__=get(f"{__pypi__}/json").json()["info"]["version"]
+# __newest__=get(f"{__pypi__}/json").json()["info"]["version"]
 
-if __newest__ != __version__:
-    print(f"""
-    New version of UserApi is out : {__newest__} (Using {__version__}
-    To Install : pip install userapi
-    Info Lib :
-    This Pkg For Telegram UserNames
-       Title : {__title__}
-       GitHub : {__github__}
-       Author : {__author__}
-       PyPi : {__pypi__}
-       CopyRight : {__copyright__}
-       """)
+# if __newest__ != __version__:
+#     print(f"""
+#     New version of UserApi is out : {__newest__} (Using {__version__}
+#     To Install : pip install userapi
+#     Info Lib :
+#     This Pkg For Telegram UserNames
+#        Title : {__title__}
+#        GitHub : {__github__}
+#        Author : {__author__}
+#        PyPi : {__pypi__}
+#        CopyRight : {__copyright__}
+#        """)
